4809681b334a543a9d89115d42cf68b29f0bf69d
Ravindra Sonawane <sonawane.ravindra1@gmail.com>
2020-10-19 17:10:17 +0530

    Working program with merging of pdf files

--- pdf_editor.py
+++ pdf_editor.py
@@ -6,9 +6,9 @@ print('Please keep the pdf files in the same folder as that of this program')
 print('Operation Codes: ')
 print('1 : Merge PDFs')
 
+os.chdir(r'C:\Users\sonaw\OneDrive\Desktop\Projects\pdf-editor-with-python')
 
 # Taking inputs from user for further processing
-file_name = input('Please provide pdf file name: ')
 operation = input('Please provide which operation you intend to perform: ')
 
 if operation == '1':
@@ -16,4 +16,12 @@ if operation == '1':
     second_file = input('Please provide name of 2nd file: ')
 
 
+file_list = [first_file, second_file]
+pdf_writer = PyPDF2.PdfFileWriter()
 
+for file_ in file_list:
+    pdf_reader = PyPDF2.PdfFileReader(file_, strict = False)
+    for page in range(pdf_reader.getNumPages()):
+        pdf_writer.addPage(pdf_reader.getPage(page))
+with open('merged_file.pdf', 'wb') as out:
+    pdf_writer.write(out)
