468da786de98c9d160d5ecddd3d1472bc5328c1d
Ravindra Sonawane <sonawane.ravindra1@gmail.com>
2020-10-19 15:32:52 +0530

    Commit for draft code containing inputs

--- pdf_editor.py
+++ pdf_editor.py
@@ -1,2 +1,19 @@
-file_name = input('Please provide pdf file name:')
-operation = input('Please provide which operation you intend to perform with the pdf file')
+import PyPDF2
+import os
+
+print('Instructions for using this program:')
+print('Please keep the pdf files in the same folder as that of this program')
+print('Operation Codes: ')
+print('1 : Merge PDFs')
+
+
+# Taking inputs from user for further processing
+file_name = input('Please provide pdf file name: ')
+operation = input('Please provide which operation you intend to perform: ')
+
+if operation == '1':
+    first_file = input('Please provide name of 1st file: ')
+    second_file = input('Please provide name of 2nd file: ')
+
+
+
